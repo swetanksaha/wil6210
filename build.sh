ac4184c330b7e347699a45362a7492e65e6b1dee
Swetank Kumar Saha <swetankk@buffalo.edu>
2018-03-26 17:58:05 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -9,4 +9,3 @@ WIL6210_INSTALL_DIR=kernel/drivers/net/wireless/ath/wil6210
 
 make -C ${KERNEL_SRC_DIR} M=${WIL6210_SRC_DIR} clean
 make -C ${KERNEL_SRC_DIR} M=${WIL6210_SRC_DIR} CONFIG_WIL6210_DEBUGFS=y modules
-sudo make -C ${KERNEL_SRC_DIR} M=${WIL6210_SRC_DIR} INSTALL_MOD_DIR=${WIL6210_INSTALL_DIR} modules_install
